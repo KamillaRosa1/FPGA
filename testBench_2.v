63c5af26e934ae490156e8ac26ba65b07cf23570
Kamilla <128093202+KamillaRosa1@users.noreply.github.com>
2024-03-04 21:57:58 -0400

    Add files via upload

---
+++ testBench_2.v
@@ -0,0 +1,32 @@
+module testBench_2;
+
+  reg [7:0] x, y;
+  reg Cin;
+  wire [7:0] A;
+  wire Cout;
+
+  // Instância do somador
+  som_comp_8bit soma_inst ( 
+        .x(x),
+        .y(y), 
+        .Cin(Cin),
+        .A(A), 
+        .Cout(Cout)
+    );
+
+ initial begin
+        x = 8'b01010101; 
+        y = 8'b11001100; 
+        Cin = 1'b1;
+
+        #10; // Espera 10 unidades de tempo
+
+        $display("Aplicando estímulos: x=%b, y=%b, Cin=%b", x, y, Cin);
+       
+        $display("Resultado: A=%b, Cout=%b", {A}, Cout); 
+
+        $finish; 
+    end
+
+endmodule
+
